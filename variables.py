b7aafc7410977f3ed2623a4f80bbd2520522ef15
antonSpirin <spirin.soft@gmail.com>
2022-11-28 11:42:07 +0300

    save

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Объект "сотрудник"
+
+name = "Roman"
+age = 35
+experience_years = 7.5
+specialization = "Driver"
+higher_education = True
+driving_experience = 10
+print('Имя: ', name, type(name))
+print('Возраст:', age, type(age))
+print('Опыт вождения:', experience_years, type(experience_years))
+print('Специальность:', specialization, type(specialization))
+print('Опыт вождения:', higher_education, type(higher_education))
+print('Опыт вождения:', driving_experience, type(driving_experience))
